da25da4433f7f6ea89b725c19fb9c3d01d6b0edc
avan31017 <avan31017@gmail.com>
2024-05-24 09:29:24 +0900

    Create Project

---
+++ TodoList_with_UIKit_CodeOnly/View/TodoListMainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TodoListMainViewController.swift
+//  TodoList_with_UIKit_CodeOnly
+//
+//  Created by 조성빈 on 5/24/24.
+//
+
+import UIKit
+
+class TodoListMainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        
+
+    }
+
+}
---
+++ TodoList_with_UIKit_CodeOnly/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TodoList_with_UIKit_CodeOnly
+//
+//  Created by 조성빈 on 5/24/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
